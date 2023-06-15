6a6dd0f29babc46e2e1e53e05fdc0c7e00fe1f17
najimiman <114479872+najimiman@users.noreply.github.com>
2023-06-15 16:03:58 +0100

    front end

--- Easy-Track/resources/views/index.blade.php
+++ Easy-Track/resources/views/index.blade.php
@@ -198,60 +198,7 @@
   </header><!-- End Header -->
 
   <!-- ======= Sidebar ======= -->
-  <aside id="sidebar" class="sidebar">
-
-    <ul class="sidebar-nav" id="sidebar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link " href="index..blade.php">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
-
-     
-
-
-      <li class="nav-heading">Pages</li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile..blade.php">
-          <i class="bi bi-person"></i>
-          <span>Profile</span>
-        </a>
-      </li><!-- End Profile Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-faq..blade.php">
-          <i class="bi bi-question-circle"></i>
-          <span>F.A.Q</span>
-        </a>
-      </li><!-- End F.A.Q Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact..blade.php">
-          <i class="bi bi-envelope"></i>
-          <span>Contact</span>
-        </a>
-      </li><!-- End Contact Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register..blade.php">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li><!-- End Register Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login..blade.php">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-      </li><!-- End Login Page Nav -->
-
-    </ul>
-
-  </aside><!-- End Sidebar-->
+  @include('sidebar')
 
   <main id="main" class="main">
 
---
+++ Easy-Track/resources/views/sidebar.blade.php
@@ -0,0 +1,60 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="index..blade.php">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+     
+
+
+      <li class="nav-heading">Pages</li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile..blade.php">
+          <i class="bi bi-list-task"></i>
+          <span>TOUT SERVICE</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile..blade.php">
+          <i class="bi bi-phone"></i>
+          <span>SERVICE MOBILE</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-faq..blade.php">
+          <i class="bi bi-telephone"></i>
+          <span>SERVICE FIXE</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-contact..blade.php">
+          <i class="bi bi-globe"></i>
+          <span>SERVICE INTERNET ADSL & MT DUO</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-register..blade.php">
+          <i class="bi bi-router"></i>
+          <span>SERVICE INTERNET FIBRE OPTIQUE</span>
+        </a>
+      </li>
+
+      {{-- <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login..blade.php">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Login</span>
+        </a>
+      </li><!-- End Login Page Nav --> --}}
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
